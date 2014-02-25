bdb639331fec0d0d9e9547839e6ce25e95b66ca5
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-02-25 19:55:09 +0100

    migrate to github

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 #' @import BBmisc
 #' @import stringr
 #' @import TSP
+#' @import MASS
+#' @import ggplot2
 #' @importFrom fpc dbscan
 #' @importFrom vegan spantree spandepth
 #' @importFrom splancs areapl
